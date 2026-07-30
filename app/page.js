dd6ed25b6365fef878b37c8772ed57d4b3c3d02b
pjcr08 <98587091+pjcr08@users.noreply.github.com>
2026-07-30 22:26:53 +0200

    Update page.js

--- app/page.js
+++ app/page.js
@@ -1,7 +1,7 @@
 "use client";
 
 import Link from 'next/link';
-import { supabase } from '../lib/supabase';
+import { supabase } from '../../lib/supabase';
 
 export default function Home() {
   return (
