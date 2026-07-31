23625c99cd6bc687070f8de12dc207ac34f61e2f
pjcr08 <98587091+pjcr08@users.noreply.github.com>
2026-07-31 18:24:48 +0200

    Update page.js

--- app/barra/page.js
+++ app/barra/page.js
@@ -1,7 +1,7 @@
 'use client';
 
 import { useEffect, useState } from 'react';
-import { supabase } from '@/lib/supabaseClient';
+import { supabase } from '@/lib/supabase'
 
 export default function BarraPage() {
   const [comandas, setComandas] = useState([]);
